f2ea192dd789e78316ca657a6cd44c822d1db878
Kenny Freeman <kefreeman@ea.com>
2016-06-22 13:28:03 +1000

    updated couchbase collector after reviewing couchbase api - included more detailed metrics, removed some duplicate metrics

--- src/collectors/couchbase/couchbase.py
+++ src/collectors/couchbase/couchbase.py
@@ -74,39 +74,78 @@ class CouchBaseCollector(diamond.collector.Collector):
       return json.load(resp)
 
 
-   def collect_bucket(self, bucket):
-      url_fmt = "http://{0}:{1}/pools/default/buckets/{2}"
-      url = url_fmt.format(self.config['host'], self.config['port'], bucket)
+   def get_pools(self):
+      ret = []
+      url_fmt = "http://{0}:{1}/pools/"
+      url = url_fmt.format(self.config['host'], self.config['port'])
+      data = self.get_data(url)
+      if "pools" in data:
+         for pool in data["pools"]:
+            if "name" in pool and "uri" in pool:
+               ret.append(pool)
+      return ret
+
+
+   def publish_bucket_basic(self, pool):
+      url_fmt = "http://{0}:{1}/pools/{2}/buckets/"
+      url = url_fmt.format(self.config['host'], self.config['port'], pool['name'])
+      data = self.get_data(url)
+      # response should be an array of dicts - we want basicStats from each
+      for bucket in data:
+         if "name" in bucket and "basicStats" in bucket:
+            name = bucket["name"]
+            stat = "basicStats"
+            for metric, val in bucket[stat].items():
+               fmetric = "{0}.buckets.basic.{1}.{2}".format(pool, name, metric)
+               self.publish(fmetric, val)
+         # also publish stats for bucket
+         self.publish_bucket_stats(pool['name'], name)
+
+
+   def publish_bucket_stats(self, pool, bucket):
+      url_fmt = "http://{0}:{1}/pools/{2}/buckets/{3}/stats"
+      url = url_fmt.format(self.config['host'], self.config['port'], pool, bucket)
       data = self.get_data(url)
 
-      rstats = [ "basicStats", "quota" ]
-      for stat in rstats:
-         if stat in data:
-            for metric, val in data[stat].items():
-               fmetric = "{0}.{1}.{2}".format(stat, bucket, metric)
-               self.publish(fmetric, val)
-         else:
-            err = "CouchBaseCollector: {0} missing from {1}\n"
-            self.log.error(err.format(stat, url))
+      if "op" in data and "samples" in data["op"]:
+         for metric, val in data["op"]["samples"].items():
+            fmetric = "{0}.buckets.stats.{1}.{2}".format(pool, bucket, metric)
+            self.publish(fmetric, val)
 
-      node_stats = "nodes"
-      if node_stats in data:
-         for node in data[node_stats]:
+
+   def publish_node_stats(self):
+      ## fix me - pool is name: url dict
+      url_fmt = "http://{0}:{1}/pools/nodes/"
+      url = url_fmt.format(self.config['host'], self.config['port'])
+      data = self.get_data(url)
+
+      stat = "storageTotals" # duplicating metrics for this one..
+      if stat in data:
+         for key in data[stat].keys():
+            for metric, val in data[stat][key].items():
+               fmetric = "{0}.node.storage.{1}.{2}".format(pool, key, metric)
+               self.publish(fmetric, val)
+
+      stat = "nodes"
+      if stat in data:
+         for node in data[stat]:
             # only record metrics for the current node
             if "thisNode" in node and node["thisNode"]:
                # record all key,val in interestingStats
                for metric,val in node["interestingStats"].items():
-                  fmetric = "istats.{0}.{1}".format(bucket, metric)
+                  fmetric = "{0}.node.stats.{1}".format(pool, metric)
                   self.publish(fmetric, val)
-               record = [ "memoryTotal", "memoryFree", "mcdMemoryReserved", "mcdMemoryAllocated" ]
-               for metric in record:
-                  fmetric = "stats.{0}.{1}".format(bucket, metric)
-                  self.publish(fmetric, val)
+
+      stat = "counters" ## duplicating data for this one as well..
+      if stat in data:
+         # record all key,val in interestingStats
+         for metric,val in node[stat].items():
+            fmetric = "{0}.node.{1}.{2}".format(pool, stat, metric)
+            self.publish(fmetric, val)
 
 
    def collect(self):
-      buckets_str = self.config['buckets'] if self.config['buckets'] else ""
-      buckets = buckets_str.split(",")
-
-      for bucket in buckets:
-         self.collect_bucket(bucket)
+      self.publish_node_stats()
+      pools = self.get_pools()
+      for pool in pools:
+         self.publish_bucket_basic(pool)
